d7e1a6fffe32a03e7171ec167e61000b870d55c5
Eastrain0610 <dws0610@naver.com>
2024-10-11 00:54:40 +0000

    2

--- pages/6 유연관계.py
+++ pages/6 유연관계.py
@@ -1,7 +1,26 @@
+import os
 import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-import networkx as nx
+
+# 폰트 파일 경로 설정: 다양한 경로에서 시도해 보기
+possible_paths = [
+    "./fonts/NanumGothic.ttf",
+    "../fonts/NanumGothic.ttf",
+    "/workspaces/test/fonts/NanumGothic.ttf"
+]
+
+font_path = None
+for path in possible_paths:
+    if os.path.exists(path):
+        font_path = path
+        break
+
+if font_path:
+    fontprop = fm.FontProperties(fname=font_path)
+else:
+    st.warning("NanumGothic.ttf 폰트 파일을 찾을 수 없습니다. 기본 폰트를 사용합니다.")
+    fontprop = None  # 폰트를 찾지 못했을 경우 기본 폰트를 사용
 
 # Streamlit 웹 페이지 설정
 st.set_page_config(page_title="사이토크롬 C 서열 비교: 사람 vs 다른 동물", layout="wide")
@@ -17,7 +36,8 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # 나눔 고딕 폰트를 Matplotlib에 설정
-plt.rc('font', family='NanumGothic')
+if fontprop:
+    plt.rc('font', **{'family': fontprop.get_name()})
 plt.rcParams['axes.unicode_minus'] = False
 
 # Streamlit 앱 설명
@@ -41,8 +61,8 @@ st.write(f"사람과 다른 동물의 사이토크롬 C 서열 일치율: {simil
 # 서열 비교 시각화
 fig, ax = plt.subplots(figsize=(10, 4))
 ax.bar(['사람', '다른 동물'], [len(human_sequence), len(other_animal_sequence)], color=['blue', 'green'])
-ax.set_ylabel('서열 길이')
-ax.set_title('사이토크롬 C 서열 길이 비교')
+ax.set_ylabel('서열 길이', fontproperties=fontprop if fontprop else None)
+ax.set_title('사이토크롬 C 서열 길이 비교', fontproperties=fontprop if fontprop else None)
 
 # Streamlit에 그래프 출력
 st.pyplot(fig)
